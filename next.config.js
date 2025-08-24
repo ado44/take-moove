4985fa2167a2cb17f4bbba782a4e00b202ab9795
Armand Marna <azor1898@gmail.com>
2025-08-24 14:43:38 +0200

    Add GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/take-move' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/take-moove' : '',
   // Permettre les connexions externes
   async headers() {
     return [
